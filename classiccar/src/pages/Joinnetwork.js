e000958958e11e05efd42a5f714eff4bf55d5f6e
Fotimafgyt <159242143+Fotimafgyt@users.noreply.github.com>
2024-04-29 15:34:26 +0500

    fotima
    sdasd

--- classiccar/src/pages/Joinnetwork.js
+++ classiccar/src/pages/Joinnetwork.js
@@ -7,7 +7,7 @@ export default function Joinnetwork() {
     <div>
       <div className={s.imgg}>
         <div className={s.black}>
-        <h1>ClassicCars.com Dealer Network <br />
+        <h1 >ClassicCars.com Dealer Network <br />
 <span>Join now to grow your business!</span>
 </h1>
         </div>
@@ -18,50 +18,57 @@ export default function Joinnetwork() {
     
 <div className={s.dealer}>
   <div className={s.network}>
-    <h1>Why the ClassicCars.com Dealer Network?</h1>
-  <div className={s.setting}><IoMdSettings className={s.set} /><h3>24/7 Inventory exposure to over 4+ million monthly visitors</h3></div>
-<div className={s.setting}><IoMdSettings className={s.set} /><h3>Trusted by over 500 modern and classic car dealers</h3></div>
+    <h1 className={s.com}>Why the ClassicCars.com Dealer Network?</h1>
+  <div className={s.setting}>
+    <IoMdSettings className={s.set} /><h3>24/7 Inventory exposure to over 4+ million monthly visitors</h3>
+    </div>
 <div className={s.setting}>
-<IoMdSettings className={s.set}/><h3>Qualified leads to turn inventory faster</h3></div>
-<div className={s.setting}><IoMdSettings className={s.set}/><h3> No contract, month to month agreement</h3></div>
-<div className={s.setting}><IoMdSettings className={s.set}/><h3>Award-winning customer support team</h3></div>
-<div className={s.setting}><IoMdSettings className={s.set}/><h3> Choice of classified listings or auction listings</h3><div className={s.new}>NEW</div></div>
+  <IoMdSettings className={s.set} /><h3>Trusted by over 500 modern and classic car dealers</h3>
+  </div>
+<div className={s.setting}>
+<IoMdSettings className={s.set}/><h3>Qualified leads to turn inventory faster</h3>
+</div>
+<div className={s.setting}>
+  <IoMdSettings className={s.set}/><h3> No contract, month to month agreement</h3>
+  </div>
+<div className={s.setting}>
+  <IoMdSettings className={s.set}/><h3>Award-winning customer support team</h3>
+  </div>
+<div className={s.setting}>
+  <IoMdSettings className={s.set}/><h3> Choice of classified listings or auction listings</h3><div className={s.new}>NEW</div>
+  </div>
 <div className={s.setting}>
 <IoMdSettings className={s.set}/><h3>Quick startup. List today</h3>
 </div>
   </div>
-  <div className={s.newfeature}>
-    <div className={s.featur}>
-      <div className={s.new2}>
-        <span>New Feature!</span>
-      </div>
-      <h1><span>The Dealer Network</span> now provides an additional way to move your inventory.</h1>
-    </div>
-    <div className={s.intro}>
-      <h1>Introducing AutoHunter</h1>
-      <h2>The premier online collector-car auction platform</h2>
-   <div className={s.collector}>
-   <div className={s.move}>
-  
-<div className={s.circle}>
-      <FaCheckCircle className={s.check}/><h3>No Seller Commission</h3></div>
-<div className={s.circle}>    <FaCheckCircle className={s.check}/><h3>Move Inventory Fast</h3></div>
-
-<div className={s.circle}>      <FaCheckCircle className={s.check}/><h3>Personalized Service</h3></div>
-      </div>
-      <div className={s.move1}>
-<div className={s.circle}>
-<FaCheckCircle className={s.check}/><h3>Listed in Days Not Weeks!t</h3>
+<div className={s.hunter}>
+<div className={s.new_feature}>
+<div className={s.df1}>
+<div className={s.new2}>New Feature!</div>
+<h1>The Dealer Network now provides an additional way to move your inventory.</h1>
 </div>
-<div className={s.circle}>
-  
-<FaCheckCircle className={s.check}/><h3>No Risk, Unsold Inventory Relists on ClassicCars.com</h3>
-</div>
-      <img src="https://classiccars.com/img/logos/AutoHunter-Live-Online-Auctions-Driven-By-Classic-Cars.gif" alt="" />
+<div className={s.intro}>
+  <div className={s.introich}>
+    <h1>Introducing AutoHunter</h1>
+    <h3>The premier online collector-car auction platform</h3>
+    <div className={s.fast}>
+      <div className={s.fast1}>
+        <div className={s.checkt}>
+        <FaCheckCircle /> <h3>Move Inventory Fast</h3>
+        </div>
+        <div className={s.checkt}>
+        <FaCheckCircle /> <h3>Move Inventory Fast</h3>
+        </div>
+        <div className={s.checkt}>
+        <FaCheckCircle /> <h3>Move Inventory Fast</h3>
+        </div>
       </div>
-   </div>
+      <div className={s.fast1}></div>
     </div>
   </div>
+</div>
+</div>
+</div>
 </div>
 
 <h1 className={s.request}>Request more information:</h1>
@@ -79,7 +86,7 @@ export default function Joinnetwork() {
     <label htmlFor="">Preferred Contact Phone Number</label><br />
     <input type="text" />
   </div>
-  <div className={s.name}>
+  <div  className={s.name}>
   <label htmlFor="">Website</label><br />
     <input type="text" /><br />
     <label htmlFor="">Average Number of Vehicles in Inventory</label><br />
